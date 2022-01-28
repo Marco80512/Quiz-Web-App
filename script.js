3a43dc7d1cb18e9a24a538401224f0e3ca27f026
P1 <Marco>
2022-01-28 09:59:18 -0800

    css

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+	$("h1").click(function() {
+		
+	});
+	
+	$("#myDiv").click(function(){
+	
+
+	});
+	$("#p1").click(function(){
+		
+	$(".disappear").fadeToggle("slow");
+  
+  });
+
+	
+
+		
+	});
+	
+	
+	
+
+		
+	
+
 
